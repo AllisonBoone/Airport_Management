e5ed74760bfc289adaa1f3051c1d0ae72a81f07b
Allison <Allison.Boone@keyin.com>
2025-02-25 16:33:10 -0330

    Added main class to airportmanagement app to run Springboot

---
+++ src/main/java/com/example/airportmanagement/AirportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.airportmanagement;
+ 
+// Added imports.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+// Created main class to run SpringBoot.
+@SpringBootApplication
+public class AirportManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AirportManagementApplication.class, args);
+    }
+}
