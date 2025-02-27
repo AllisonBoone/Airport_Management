61d62320ab8d6988fd99024bfcb6e0ed99ebd6a0
Allison <Allison.Boone@keyin.com>
2025-02-27 18:31:55 -0330

    Added passenger repository

--- src/main/java/com/example/airportmanagement/controller/PassengerController.java
+++ src/main/java/com/example/airportmanagement/controller/PassengerController.java
@@ -1,5 +1,53 @@
 package com.example.airportmanagement.controller;
 
+// Added imports.
+import com.example.airportmanagement.model.Passenger;
+import com.example.airportmanagement.service.PassengerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+// Created controller for passenger api.
+@RestController
+@RequestMapping("/passengers")
 public class PassengerController {
+    private final PassengerService passengerService;
+
+    public PassengerController(PassengerService passengerService) {
+        this.passengerService = passengerService;
+    }
+
+    // Get all passengers.
+    @GetMapping
+    public List<Passenger> getAllPassengers() {
+        return passengerService.getAllPassengers();
+    }
+
+    // Get passengers by ID>
+    @GetMapping("/{id}")
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable Long id) {
+        Optional<Passenger> passenger = passengerService.getPassengerById(id);
+        return passenger.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Add passenger.
+    @PostMapping
+    public ResponseEntity<Passenger> addPassenger(@RequestBody Passenger passenger) {
+        return new ResponseEntity<>(passengerService.addPassenger(passenger), HttpStatus.CREATED);
+    }
+
+    // Update passenger.
+    @PutMapping("/{id}")
+    public ResponseEntity<Passenger> updatePassenger(@PathVariable Long id, @RequestBody Passenger passenger) {
+        return ResponseEntity.ok(passengerService.updatePassenger(id, passenger));
+    }
+
+    // Delete existing passenger.
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePassenger(@PathVariable Long id) {
+        passengerService.deletePassenger(id);
+        return ResponseEntity.noContent().build();
+    }
     
 }
--- src/main/java/com/example/airportmanagement/repository/PassengerRepository.java
+++ src/main/java/com/example/airportmanagement/repository/PassengerRepository.java
@@ -1,5 +1,12 @@
 package com.example.airportmanagement.repository;
 
-public class PassengerRepository {
-    
+// Added imports.
+import com.example.airportmanagement.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+// Created repository for doing database operations on passenger.
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+    Optional<Passenger> findByFirstNameAndLastName(String firstName, String lastName);
 }
+
