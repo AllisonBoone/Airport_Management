0446607ff2a80ec54af3bce416407893e4b9457b
Allison <Allison.Boone@keyin.com>
2025-02-27 17:34:48 -0330

    Added findByName optional in airportrepository.java

--- src/main/java/com/example/airportmanagement/repository/AirportRepository.java
+++ src/main/java/com/example/airportmanagement/repository/AirportRepository.java
@@ -4,9 +4,11 @@ package com.example.airportmanagement.repository;
 import com.example.airportmanagement.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
  
 
 // Created repository for airport database operations.
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    Optional<Airport> findByName(String name);
     List<Airport> findByCityId(Long cityId);
 }
